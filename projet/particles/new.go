66d39bd344933c8fa0c0250fedf5f4629cfdaddf
Justine BERNIER <E228120G@univ-nantes.fr>
2023-01-03 10:57:13 +0100

    The number of particles is defined according to the parameter InitNumParticle and the particles appear with random positions when the parameter RandomSpawn is true, otherwise appear with the position defined by the parameters SpawnX and SpawnY.

--- projet/particles/new.go
+++ projet/particles/new.go
@@ -3,6 +3,8 @@ package particles
 import (
 	"container/list"
 	"project-particles/config"
+	"math/rand"
+	"time"
 )
 
 // NewSystem est une fonction qui initialise un système de particules et le
@@ -12,12 +14,22 @@ import (
 // centre de l'écran.
 func NewSystem() System {
 	l := list.New()
-	l.PushFront(&Particle{
-		PositionX: float64(config.General.WindowSizeX) / 2,
-		PositionY: float64(config.General.WindowSizeY) / 2,
-		ScaleX:    1, ScaleY: 1,
-		ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
-		Opacity: 1,
-	})
+	var PosX = float64(config.General.SpawnX)
+	var PosY = float64(config.General.SpawnY)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0 ; i < config.General.InitNumParticles ; i++{
+		if config.General.RandomSpawn{
+			PosX = rand.Float64() * (float64(config.General.WindowSizeX))
+			PosY = rand.Float64() * (float64(config.General.WindowSizeY))
+		}
+		l.PushFront(&Particle{
+			PositionX: PosX,
+			PositionY: PosY,
+			ScaleX:    1, ScaleY: 1,
+			ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
+			Opacity: 1,
+		})
+	}
+
 	return System{Content: l}
 }
