86dd3cc1911482bbf941511684470834b42c853e
Basma <basmamalki22@gmail.com>
2023-01-08 01:54:07 +0100

    Added test for Update(), check if the speed is good.

--- projet/particles/new.go
+++ projet/particles/new.go
@@ -7,9 +7,8 @@ import (
 
 // NewSystem est une fonction qui initialise un système de particules et le
 // retourne à la fonction principale du projet, qui se chargera de l'afficher.
-// C'est à vous de développer cette fonction.
-// Dans sa version actuelle, cette fonction affiche une particule blanche au
-// centre de l'écran.
+// Cette fonction NewSystem permet de créer le nombre de particules qui est precisés dans le paramétre InitNumParticles qui se trouve dans le fichier config.json
+
 func NewSystem() System {
 	l := list.New()
 	for i := 0; i < config.General.InitNumParticles; i++ {
--- projet/particles/update_test.go
+++ projet/particles/update_test.go
@@ -1,13 +1,28 @@
 package particles
 
-/*
+import (
+	"container/list"
+	"project-particles/config"
+	"testing"
+)
+
 func TestUpdate(t *testing.T) {
 	//Test par exemple si position de x a bougé
 	//là je ne test pas que la particule a bougé par rapport a la position initiale
 	//sauf que rand des fois a 0
+	config.General.InitNumParticles = 1
+	config.General.TypeSpeed = 1
 	config.General.MaxSpeed = 10
-	if ..., ... == Update().e.positionX, Update().e.positionX {
+	var l *list.List = list.New()
+	l.PushFront(ParticuleCr())
+	var SystemParticle *System = &System{Content: l}
+	var particule *Particle = l.Front().Value.(*Particle)
+	var posX float64 = particule.PositionX
+	var posY float64 = particule.PositionY
+
+	SystemParticle.Update()
+
+	if posX == SystemParticle.Content.Front().Value.(*Particle).PositionX || posY == SystemParticle.Content.Front().Value.(*Particle).PositionY {
 		t.Error("Les particules ne se déplace pas et pourtant elle devrait.")
 	}
 }
-*/
